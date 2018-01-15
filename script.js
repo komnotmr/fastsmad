ff5662ec58a8210fb7453d04c6b8e34869a4b112
komnotmr <komnotmr@gmail.com>
2018-01-15 21:18:10 +0700

    add input

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+var models = {
+    '1': (x)=>{return 1.0; },
+    'x': (x)=>{return x; },
+    'xx': (x)=>{return x*x; },
+    'xxx': (x)=>{return x*x*x; }
+}
+
 function input() {
     console.log('input');
-    let _x = [];
-    let _y = [];
     let x = document.getElementById('x').value.replace(/ /g, '').split(',');
     let y = document.getElementById('y').value.replace(/ /g, '').split(',');
+    let m = document.getElementById('m').value.replace(/ /g, '').split(',');
+    let _s = document.getElementById('s').value.replace(/ /g, '');
+    let _x = [];
+    let _y = [];
+    let _m = [];
+
     for (let i = 0; i < x.length; i++) { _x.push(Number(x[i])); }
     for (let i = 0; i < y.length; i++) { _y.push(Number(y[i])); }
-    return [_x, _y];
+    for (let i = 0; i < m.length; i++) { _m.push(models[m[i]]); }
+
+    return [_x, _y, Number(_s), _m];
 }
 
 function make_table() {
